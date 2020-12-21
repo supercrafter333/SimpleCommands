6124c276ae9d4032ff9f84403d4be99c38ff9f1f
Hannes <65465954+HannesTheDev@users.noreply.github.com>
2020-12-21 14:52:18 +0100

    Update Main.php

--- src/HannesTheDev/Main.php
+++ src/HannesTheDev/Main.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace HannesTheDev;
+
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\level\Position;
+use pocketmine\command\Command;
+use HannesTheDev\Commands\FeedCommand;
+use HannesTheDev\Commands\FlyCommand;
+use HannesTheDev\Commands\HealCommand;
+use HannesTheDev\Commands\TagCommand;
+use HannesTheDev\Commands\NachtCommand;
+use HannesTheDev\Commands\SimpleCommandsCommand;
+
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getLogger->info("Plugin aktiviert");
+    $this->getServer->getPluginManager->registerEvents($this, $this);
+  }
+  
+  public function onDisable(){
+    $this->getLogger->info("Plugin deaktiviert");
+  }
+}
